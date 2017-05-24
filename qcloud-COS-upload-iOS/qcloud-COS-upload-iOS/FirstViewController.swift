3f9df2ddbb00022b88981b02247095cf7f1044c8
Kevin_Feng <Kevin_Feng@139.com>
2017-05-24 18:45:38 +0800

    init

---
+++ qcloud-COS-upload-iOS/qcloud-COS-upload-iOS/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  qcloud-COS-upload-iOS
+//
+//  Created by Kevin_Feng on 2017/5/24.
+//  Copyright (c) 2017 Kevin_Feng. All rights reserved.
+//
+
+import UIKit
+
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
